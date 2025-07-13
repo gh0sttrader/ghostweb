beacfa2a61f21c8e5b7ed60d7fec600cc6b99a92
Josh Bajorek <milk18001955@gmail.com>
2025-07-13 21:47:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/InteractiveChartCard.tsx
+++ src/components/InteractiveChartCard.tsx
@@ -1,6 +1,6 @@
 
 "use client"
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
 import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts';
