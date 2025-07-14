06633bfc78143077c409df663c228a863cb10029
Josh Bajorek <milk18001955@gmail.com>
2025-07-14 01:46:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/icons/GhostIcon.tsx
+++ src/components/icons/GhostIcon.tsx
@@ -6,14 +6,10 @@ export const GhostIcon = (props: React.SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
     height="1em"
-    fill="none"
-    stroke="currentColor"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="16"
+    fill="currentColor"
     viewBox="0 0 256 256"
     {...props}
   >
-    <path d="M96,128a16,16,0,1,1,16-16A16,16,0,0,1,96,128Zm64,0a16,16,0,1,0-16-16A16,16,0,0,0,160,128Zm-32-8a104,104,0,0,0-88,48v40a8,8,0,0,0,8,8h40v-8a16,16,0,0,1,16-16h48a16,16,0,0,1,16,16v8h40a8,8,0,0,0,8-8v-40A104,104,0,0,0,128,120Z" />
+    <path d="M128,24A104,104,0,0,0,24,128v88a16,16,0,0,0,16,16H80a16,16,0,0,0,16-16V216a16,16,0,0,1,16-16h48a16,16,0,0,1,16,16v8a16,16,0,0,0,16,16h40a16,16,0,0,0,16-16V128A104,104,0,0,0,128,24Zm40,120a16,16,0,1,1,16-16A16,16,0,0,1,168,144Zm-64,0a16,16,0,1,1,16-16A16,16,0,0,1,104,144Z" />
   </svg>
 );
