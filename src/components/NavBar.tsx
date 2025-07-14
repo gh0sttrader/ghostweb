62f5a85c2b262080968bf691c4d43da947b7361e
Josh Bajorek <milk18001955@gmail.com>
2025-07-14 01:52:18 +0000

    Detailed Prompt to Fix Ghost Logo Using This as a Reference TASK: Replac

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Input } from "./ui/input";
 import { Search } from "lucide-react";
+import { GhostIcon } from "./GhostIcon";
 
 export function NavBar() {
   const pathname = usePathname();
@@ -30,16 +31,7 @@ export function NavBar() {
         {/* Left: Logo only */}
         <div className="flex items-center">
           <Link href="/trading" className="flex items-center space-x-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              fill="currentColor"
-              viewBox="0 0 256 256"
-              className="h-6 w-6 text-primary"
-            >
-              <path d="M128,24A104,104,0,0,0,24,128v88a16,16,0,0,0,16,16H80a16,16,0,0,0,16-16V216a16,16,0,0,1,16-16h48a16,16,0,0,1,16,16v8a16,16,0,0,0,16,16h40a16,16,0,0,0,16-16V128A104,104,0,0,0,128,24Zm40,120a16,16,0,1,1,16-16A16,16,0,0,1,168,144Zm-64,0a16,16,0,1,1,16-16A16,16,0,0,1,104,144Z" />
-            </svg>
+            <GhostIcon className="h-6 w-6 text-primary" />
           </Link>
         </div>
 
