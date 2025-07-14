90e3349693b81674bb5a9ac7d81fecb0aed91f3f
Josh Bajorek <milk18001955@gmail.com>
2025-07-14 01:49:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,6 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { GhostIcon } from "@/components/icons/GhostIcon";
 import { Input } from "./ui/input";
 import { Search } from "lucide-react";
 
@@ -31,7 +30,16 @@ export function NavBar() {
         {/* Left: Logo only */}
         <div className="flex items-center">
           <Link href="/trading" className="flex items-center space-x-2">
-            <GhostIcon className="h-6 w-6 text-primary" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              fill="currentColor"
+              viewBox="0 0 256 256"
+              className="h-6 w-6 text-primary"
+            >
+              <path d="M128,24A104,104,0,0,0,24,128v88a16,16,0,0,0,16,16H80a16,16,0,0,0,16-16V216a16,16,0,0,1,16-16h48a16,16,0,0,1,16,16v8a16,16,0,0,0,16,16h40a16,16,0,0,0,16-16V128A104,104,0,0,0,128,24Zm40,120a16,16,0,1,1,16-16A16,16,0,0,1,168,144Zm-64,0a16,16,0,1,1,16-16A16,16,0,0,1,104,144Z" />
+            </svg>
           </Link>
         </div>
 
