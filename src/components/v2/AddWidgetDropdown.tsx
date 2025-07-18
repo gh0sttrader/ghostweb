61fedf952473030202fb96664c64d73c315221fa
Josh Bajorek <milk18001955@gmail.com>
2025-07-18 22:20:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/v2/AddWidgetDropdown.tsx
+++ src/components/v2/AddWidgetDropdown.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import React, 'react';
+import * as React from 'react';
 import { Button } from '@/components/ui/button';
 import { ChartBar, Newspaper, ScanSearch, Table2, ShoppingCart } from 'lucide-react';
 
