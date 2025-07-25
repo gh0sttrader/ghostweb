fa647f1355000053631998dbfe1119c0a5c26142
Josh Bajorek <milk18001955@gmail.com>
2025-07-25 23:09:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/tradehistory/page.tsx
+++ src/app/(app)/tradehistory/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import React, 'react';
+import React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Calendar } from "@/components/ui/calendar";
 import { cn } from "@/lib/utils";
