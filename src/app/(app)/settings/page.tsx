771a6b98a1b5617b3e853c4dcdee5a8df79c9399
Josh Bajorek <milk18001955@gmail.com>
2025-08-03 01:17:40 +0000

    Prompt for Gemini/devs
    pgsql
    Copy
    Edit
    On the Settings page, **remove t

--- src/app/(app)/settings/page.tsx
+++ src/app/(app)/settings/page.tsx
@@ -1,13 +1,5 @@
-"use client";
-
-import dynamic from 'next/dynamic';
-
-const TradingViewWidget = dynamic(() => import('@/components/TradingViewWidget'), { ssr: false });
 
 export default function SettingsPage() {
-  return (
-    <main className="w-full h-full bg-black">
-      <TradingViewWidget />
-    </main>
-  );
+  // Completely blank settings page for now
+  return <></>;
 }
